1044c1b8e8bab15c074fa20e1f519d875d8ce3b8
pallavi mahanor <pallavimaharnor7710@gmail.com>
2024-08-12 18:17:39 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,43 @@ package com.driver;
 
 public class Main {
 
+    // task 1 = create a class product inside main
+    static class product {
+        // tassk 3 = : Create a method
+        public int product(int x, int y) {
+            return x * y;
+        }
+        // task 4 = overloading method
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        // task 5 = overloading method
+        public double product(double x, double y) {
+            return x * y;
+        }
+
+
+    }
+
+    // task 2 = Create object of Product in Main function called p
+    public static void main(String[] args) {
+        product p = new product();
+
+        // call the method with two integer
+        int result1 = p.product(5,6);
+        System.out.println("product of 5 and 6 : " + result1);
+
+
+        // call the method
+        int result2 = p.product(2,5,6);
+        System.out.println("product of 2 , 5,6 : " + result2);
+
+        // call the method
+        double result3 = p.product(5.55,5.88);
+        System.out.println("product of 5.55 and 5.88 : " + result3);
+
+
+
+
+    }
 }
